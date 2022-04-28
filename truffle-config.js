82467c6bd25c4d7a7e68ebf216752b8fae7e26df
Oliver Murray <o.murray@oceans.ubc.ca>
2022-04-28 14:34:21 -0700

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,57 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config()
+
+const private_key = process.env.PRIVATE_KEY
+const matic_url = process.env.MATIC_RPC_URL
+
+
+module.exports = {
+  networks: {
+    binance_testnet: {
+      provider: () => new HDWalletProvider(private_key, 'https://data-seed-prebsc-1-s1.binance.org:8545'),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider(private_key, kovan_url)
+      },
+      network_id: '42',
+      skipDryRun: true
+    },
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(private_key, rinkeby_url)
+      },
+      network_id: '4',
+      skipDryRun: true
+    },
+    matic: {
+      provider: () => {
+        return new HDWalletProvider(private_key, matic_url)
+      },
+      network_id: '137',
+      skipDryRun: true
+    },
+    mumbai: {
+      provider: () => {
+        return new HDWalletProvider(private_key, 'https://matic-mumbai.chainstacklabs.com')
+      },
+      network_id: '80001',
+      skipDryRun: true
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.6.6',
+    },
+  },
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ]
+}
